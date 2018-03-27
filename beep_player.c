c136fe3d0b5d80bb7ccefc75546d5722564ad64b
Stefan Devai <stedevai@gmail.com>
2018-03-27 14:05:52 +0200

    Defined notes frequencies and values

--- beep_player.c
+++ beep_player.c
@@ -4,6 +4,43 @@
 #include <linux/kd.h>
 #include <unistd.h>
 
+#define SEMIBREVE 1500
+
+#define DUPLEX_LONGA SEMIBREVE*8
+
+#define LONGA SEMIBREVE*4
+
+#define BREVE SEMIBREVE*2
+
+#define MINIM SEMIBREVE/2
+
+#define CROTCHET SEMIBREVE/4
+#define CROTCHET_DOTTED SEMIBREVE/4+QUAVER
+
+#define QUAVER SEMIBREVE/8
+
+#define SEMIQUAVER SEMIBREVE/16
+
+#define DEMISEMIQUAVER SEMIBREVE/32
+
+#define HEMIDEMISEMIQUAVER SEMIBREVE/64
+
+
+#define C03 131
+#define D03 147
+#define E03 164
+#define F03 174
+#define G03 196
+#define A03 220
+#define B03 247
+#define C04 262
+#define D04 294
+#define E04 330
+#define F04 349
+#define G04 392
+#define A04 440
+#define B04 494
+
 int init_player(int *fd);
 int close_player(int *fd);
 void play_note(int *fd, int freq, int duration);
@@ -13,8 +50,8 @@ int main(int argc, char *argv[])
 	int fd;
 	init_player(&fd);
 	
-	play_note(&fd, 196, 1000);
-	play_note(&fd, 261, 1000);
+	play_note(&fd, G03, QUAVER);
+	play_note(&fd, C04, CROTCHET_DOTTED);
 
 	close_player(&fd);
 	return 0;
